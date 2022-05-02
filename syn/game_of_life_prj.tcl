32a67954adf527a900b3c4443db3f5f5a3e082f4
PhilipL <26860644+Philiplam97@users.noreply.github.com>
2022-05-02 20:56:18 +1000

    Set paths relative to src_dir variable and use file join to build paths

--- syn/game_of_life_prj.tcl
+++ syn/game_of_life_prj.tcl
@@ -1,7 +1,11 @@
-# add_files -norecurse [file dirname [info script]]/../src/chirp_gen/scripts/chirp_rom_data.txt
+set src_dir [file join [file dirname [info script]] .. src]
+set scripts_dir [file join [file dirname [info script]] .. scripts]
 
-read_vhdl -vhdl2008 [file dirname [info script]]/../src/bram_shift_reg.vhd
-read_vhdl -vhdl2008 [file dirname [info script]]/../src/VGA/VGA.vhd
-read_vhdl -vhdl2008 [file dirname [info script]]/../src/game_of_life.vhd
+read_vhdl -vhdl2008 [file join $src_dir bram_shift_reg.vhd]
+read_vhdl -vhdl2008 [file join $src_dir game_of_life.vhd]
+read_vhdl -vhdl2008 [file join $src_dir VGA VGA.vhd]
+read_vhdl -vhdl2008 [file join $src_dir common debouncer.vhd]
+read_vhdl -vhdl2008 [file join $src_dir common sync_2FF.vhd]
+read_vhdl -vhdl2008 [file join $src_dir arty_top.vhd]
 
-
+add_files -norecurse [file join $scripts_dir bram_init.txt]
